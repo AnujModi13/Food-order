1e045560ba3678cb2b2683e890ceca5200fa8404
Anuj <modianuj1376@gmail.com>
2023-11-06 23:51:03 +0530

    Added Dynamic Count and updated Data

--- src/app/service/food.service.ts
+++ src/app/service/food.service.ts
@@ -1,6 +1,6 @@
+import { tags } from './../shared/models/tags';
 import { Injectable } from '@angular/core';
 import { Foods } from '../shared/models/food';
-import { tags } from '../shared/models/tags';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,11 @@ import { tags } from '../shared/models/tags';
 export class FoodService {
 
   constructor() { }
+
+  getCount(tags: string): number {
+    return this.getAll().filter(food => food.tags?.includes(tags)).length;
+  }
+
   getFoodById(id:number):Foods
   {
    return this.getAll().find(food =>food.id ==id)!;
@@ -20,15 +25,15 @@ export class FoodService {
   getAllTags() :tags[]
   {
     return[
-      { name:'All',count:20},
-      { name:'FastFood',count:0},
-      { name:'Lunch',count:0},
-      { name:'Dinner',count:0},
-      { name:'Breakfast',count:0},
-      { name:'Dessert',count:0},
-      { name:'Snacks',count:0},
-      { name:'Drinks',count:0},
-      { name:'Salad',count:0},
+      { name:'All',count:this.getAll().length},
+      { name:'FastFood',count:this.getCount('FastFood')},
+      { name:'Lunch',count:this.getCount('Lunch')},
+      { name:'Dinner',count:this.getCount('Dinner')},
+      { name:'Breakfast',count:this.getCount('Breakfast')},
+      { name:'Dessert',count:this.getCount('Dessert')},
+      { name:'Snacks',count:this.getCount('Snacks')},
+      { name:'Drinks',count:this.getCount('Drinks')},
+      { name:'Salad',count:this.getCount('Salad')},
     ]
   }
   getAll() : Foods[]
@@ -38,89 +43,100 @@ export class FoodService {
       id:1,
       name:'Potato Meat',
       cookTime:'30-40',
-      price:230,
+      price:360,
       favourite:true,
-      origins:['India','Dubai'],
-      star:4.5,
+      origins:['India','Dubai','Afghanistan'],
+      star:4,
       imageUrl:'/assets/Image-1.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['Lunch','Dinner'],
     },
     {
       id:2,
-      name:'Potato Meat',
-      cookTime:'30-40',
-      price:230.45,
+      name:'Straberry Pasteries',
+      cookTime:'15-20',
+      price:250,
       favourite:true,
-      origins:['India','Dubai'],
-      star:3.5,
+      origins:['France', 'Italy', 'Spain'],
+      star:5,
       imageUrl:'/assets/Image-2.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['Dessert','FastFood','Breakfast'],
     },
     {
       id:3,
-      name:' Rice',
-      cookTime:'30-40',
+      name:'Greek Rice',
+      cookTime:'10-20',
       price:230,
       favourite:false,
-      origins:['India','Dubai'],
-      star:2.5,
+      origins:['Greek','France','Germany'],
+      star:3,
       imageUrl:'/assets/Image-3.jpg',
       tags:['FastFood','Lunch','Dinner'],
     },
     {
       id:4,
-      name:'Potato Meat',
+      name:'Capricciosa Pizza',
       cookTime:'30-40',
-      price:230,
+      price:470,
       favourite:true,
-      origins:['India','Dubai'],
-      star:0.5,
+      origins:['Egypt', 'Roman','Greek'],
+      star:4,
       imageUrl:'/assets/Image-4.jpg',
-      tags:['FastFood','Dinner'],
+      tags:['FastFood','Dinner','Snacks'],
     },
     {
       id:5,
-      name:'Potato Meat',
-      cookTime:'30-40',
-      price:230,
-      favourite:true,
-      origins:['India','Dubai'],
-      star:1.5,
+      name:' Kothey momo',
+      cookTime:'20-30',
+      price:180,
+      favourite:false,
+      origins:['India','Nepal','Bhutan'],
+      star:2,
       imageUrl:'/assets/Image-5.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['FastFood','Lunch','Dinner','Salad']
     },
     {
       id:6,
-      name:'Potato Meat',
-      cookTime:'30-40',
-      price:230,
+      name:'Wingreens Farm Burger',
+      cookTime:'15-20',
+      price:150,
       favourite:true,
-      origins:['India','Dubai'],
-      star:5,
+      origins:['United States','Germany','Mexico'],
+      star:4,
       imageUrl:'/assets/Image-6.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['FastFood','Lunch','Dinner','Breakfast','Snacks'],
     },
     {
       id:7,
-      name:'Potato Meat',
+      name:'Massaged Kale Salad',
       cookTime:'30-40',
-      price:230,
+      price:330,
       favourite:true,
-      origins:['India','Dubai'],
-      star:3.5,
+      origins:['India','Mexican','Australia'],
+      star:3,
       imageUrl:'/assets/Image-7.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['FastFood','Lunch','Dinner','Salad'],
     },
     {
       id:8,
-      name:'Potato Meat',
-      cookTime:'30-40',
-      price:230,
-      favourite:true,
-      origins:['India','Dubai'],
-      star:4.1,
+      name:'Idli Sambar',
+      cookTime:'10-20',
+      price:120,
+      favourite:false,
+      origins:['India','Sri Lanka','Maldives'],
+      star:5,
       imageUrl:'/assets/Image-8.jpg',
-      tags:['FastFood','Lunch','Dinner'],
+      tags:['FastFood','Dinner','Breakfast'],
+    },
+    {
+      id:9,
+      name:' Mushroom Cocktail ',
+      cookTime:'10-20',
+      price:200,
+      favourite:true,
+      origins:['England','Australia','New Zealand'],
+      star:5,
+      imageUrl:'/assets/Image-9.jpg',
+      tags:['Dinner','Drinks'],
     }
    ]
   }
